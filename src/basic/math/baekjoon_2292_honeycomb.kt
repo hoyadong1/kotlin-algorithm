1683044a934d4a2dce9f6ade2d016ee7362b7dfc
hoyadong1 <8645465as@gmail.com>
2026-02-28 22:44:46 +0900

    feat: 벌집 문제 해결

---
+++ src/basic/math/baekjoon_2292_honeycomb.kt
@@ -0,0 +1,16 @@
+package basic.math
+
+fun main(){
+    var n = readln().toInt()
+    var count = 1
+    var num = 1
+
+    while(true){
+        if(n <= num) break
+
+        num += 6*count
+        count++
+    }
+
+    print(count)
+}
