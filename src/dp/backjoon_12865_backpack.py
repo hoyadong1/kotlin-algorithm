32cd7d6518b6a5387567b629b85190b3ecfa3b5b
hoyadong1 <8645465as@gmail.com>
2026-04-05 11:53:22 +0900

    feat: 배낭 채우기 dp 문제 해결

---
+++ src/dp/backjoon_12865_backpack.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [0] * (K + 1)
+
+for w, v in items:
+    for j in range(K, w - 1, -1):
+        dp[j] = max(dp[j], dp[j - w] + v)
+
+print(dp[K])
